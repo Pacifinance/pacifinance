6ba99daa469212d81dcfa19e1007b9a3616b34e5
Alessio Bollea <26738451+BolleA7X@users.noreply.github.com>
2024-01-28 16:54:41 +0100

    Now the db object has access to the delqueue module exports

--- db/mongo.js
+++ db/mongo.js
@@ -3,9 +3,10 @@ const users = require("./models/users.js");
 const balances = require("./models/balances.js");
 const expenses = require("./models/expenses.js");
 const tags = require("./models/tags.js")
+const delqueue = require("./models/delqueue.js");
 
 async function connect(uri) {
     return mongoose.connect(uri);
 }
 
-module.exports = {connect, users, balances, expenses, tags};
+module.exports = {connect, users, balances, expenses, tags, delqueue};

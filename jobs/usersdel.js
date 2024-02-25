e63b74c78132c00b02db354ced7d915dfccf2859
Alessio Bollea <26738451+BolleA7X@users.noreply.github.com>
2024-02-25 17:11:27 +0100

    Completed implementation of users deletion job

--- jobs/usersdel.js
+++ jobs/usersdel.js
@@ -3,10 +3,22 @@ const delqueue = require("../db/models/delqueue.js");
 const expenses = require("../db/models/expenses.js");
 const users = require("../db/models/users.js");
 
+/**
+ * Checks if all user data has been deleted
+ * @param {String} user_ref - ObjectId of the user
+ * @returns true if all user data has been deleted, false otherwise
+ */
 async function checkDeletion(user_ref) {
-
+    const userExists = await users.userExistsByRef(user_ref);
+    const balancesExist = await balances.balancesExistByUserRef(user_ref);
+    const expensesExist = await expenses.expensesExistByUserRef(user_ref);
+    return ((!userExists) && (!balancesExist) && (!expensesExist));
 }
 
+/**
+ * Deletes all data of a user from the DB
+ * @param {String} user_ref - ObjectId of the user
+ */
 async function deleteUserData(user_ref) {
     // Delete user account and all its associated data
     await users.deleteUserByRef(user_ref);
@@ -14,7 +26,10 @@ async function deleteUserData(user_ref) {
     await expenses.deleteExpensesByUserRef(user_ref);
 }
 
-async function deleteUsers() {
+/**
+ * Periodic job that removes all data of users that must be deleted
+ */
+async function deleteUsersJob() {
     // Get all users in the deletion queue
     const users = await delqueue.getAllAccountsInQueue();
     // Check all users in the queue
@@ -33,5 +48,5 @@ async function deleteUsers() {
 }
 
 module.exports = {
-    deleteUsers,
+    deleteUsersJob,
 };
